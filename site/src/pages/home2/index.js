12c66d7f5515fdbe44d88497f94dfd4894608301
igorLAO <igooli670@gmail.com>
2023-09-27 11:28:32 -0300

    THE END pt.2

--- site/src/pages/home2/index.js
+++ site/src/pages/home2/index.js
@@ -48,8 +48,22 @@ export default function Post_HomePage() {
                
 
                     <div className='imagePost'>
-                        <img src={PostImage} />
                         
+                    <div className='headerResponsivo'>
+                            <span style={{ display: "flex", alignItems: "center" }}>
+                            <img src={perfil} className='imgperfil' />
+                                <a className='PerfilLink' >
+
+                                    <b onClick={Hide}> Dalva's Giveaway </b>
+                                    <svg aria-label="Verificado" class="x1lliihq x1n2onr6" color="rgb(0, 149, 246)" fill="rgb(0, 149, 246)" height="12" role="img" viewBox="0 0 40 40" width="12"><title>Verificado</title><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Zm7.415 11.225 2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill-rule="evenodd"></path></svg>
+                                    <span style={{ marginLeft: 5 }}>•</span>
+                                    <a onClick={Hide}> Seguir </a>
+                                </a>
+                            </span>
+                            <i style={{ fontSize: 20 }} class="fa-solid fa-ellipsis"></i>
+                        </div>
+                        <img src={PostImage} />
+
                     </div>
                     <div className='DetailsPost'>
 
@@ -115,7 +129,7 @@ export default function Post_HomePage() {
 
                                 <div className='textComment'>
                                     <a>
-                                        <b> Jair da silva ferreira </b> Eu ganhei eu ganheiii. Ganhar agora.
+                                        <b> Luiz Decimus </b> Eu mereço esse PC
                                     </a>
                                 </div>
                             </div>
@@ -135,7 +149,7 @@ export default function Post_HomePage() {
 
                                 <div className='textComment'>
                                     <a>
-                                        <b> Jair da silva ferreira </b> Eu ganhei eu ganheiii. Ganhar agora.
+                                        <b> Flavio Dino </b> EU irei ganhar AAAAAAAAAA.
                                     </a>
                                 </div>
                             </div>
@@ -144,7 +158,7 @@ export default function Post_HomePage() {
 
                                 <div className='textComment'>
                                     <a>
-                                        <b> Jair da silva ferreira </b> Eu ganhei eu ganheiii. Ganhar agora.
+                                        <b> Carl Young </b> È preciso imaginar sisifo ganhando esse PC Gamer
                                     </a>
                                 </div>
                             </div>
@@ -153,7 +167,7 @@ export default function Post_HomePage() {
 
                                 <div className='textComment'>
                                     <a>
-                                        <b> Jair da silva ferreira </b> Eu ganhei eu ganheiii. Ganhar agora.
+                                        <b> Kubrick </b> Imagina se eu ganhasse.
                                     </a>
                                 </div>
                             </div>
@@ -162,7 +176,7 @@ export default function Post_HomePage() {
 
                                 <div className='textComment'>
                                     <a>
-                                        <b> Jair da silva ferreira </b> Quero ganhar. Forte forte pra da sorte :{')'}.
+                                        <b> Ramon Dino </b> Quero ganhar. Forte forte pra da sorte :{')'}.
                                     </a>
                                 </div>
                             </div>
